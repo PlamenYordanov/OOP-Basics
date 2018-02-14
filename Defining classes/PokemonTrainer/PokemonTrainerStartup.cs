f687916b88523c22bbcb81f783dd2262adc9eee9
PlamenYordanov <yordanov.plamen@abv.bg>
2018-02-14 19:09:42 +0200

    pokemon 100/100

---
+++ Defining classes/PokemonTrainer/PokemonTrainerStartup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PokemonTrainerStartup
+{
+    public static void Main()
+    {
+        string input = string.Empty;
+        var trainers = new List<Trainer>();
+        while ((input = Console.ReadLine()) != "Tournament")
+        {
+            var data = input.Split(new[] { ' ' }
+            , StringSplitOptions.RemoveEmptyEntries);
+
+            var trainerName = data[0];
+
+            if (!trainers.Any(t => t.Name == trainerName))
+            {
+                var tempTrainer = new Trainer(trainerName);
+                trainers.Add(tempTrainer);
+            }
+
+            var trainer = trainers.SingleOrDefault(t => t.Name == trainerName);
+
+            AddPokemonToTrainer(data, trainer);
+        }
+
+        while ((input = Console.ReadLine()) != "End")
+        {
+            var element = input;
+
+            foreach (var trainer in trainers)
+            {
+                if (!trainer.Pokemons.Any(p => p.Element == element))
+                {
+                    trainer.ReducePokemonsHealth();
+                    continue;
+                }
+                trainer.AddBadge();
+            }
+        }
+        trainers.OrderByDescending(t => t.GetBadgeCount).ToList().ForEach(x => Console.WriteLine(x));
+    }
+
+    private static void AddPokemonToTrainer(string[] data, Trainer trainer)
+    {
+        var pokemonName = data[1];
+        var element = data[2];
+        var health = int.Parse(data[3]);
+        var pokemon = new Pokemon(pokemonName, element, health);
+        trainer.AddPokemon(pokemon);
+    }
+}
+
---
+++ Defining classes/PokemonTrainer/Trainer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Trainer
+{
+    private string name;
+
+    private int badges = 0;
+
+    private List<Pokemon> pokemons = new List<Pokemon>();
+
+    public string Name { get { return this.name; } }
+
+    public ICollection<Pokemon> Pokemons { get { return this.pokemons; } } 
+
+    public Trainer(string name)
+    {
+        this.name = name;
+    }
+    public void AddBadge()
+    {
+        this.badges++;
+    }
+    public int GetBadgeCount => this.badges;
+
+    public void AddPokemon(Pokemon pokemon)
+    {
+        this.pokemons.Add(pokemon);
+    }
+
+    public  void ReducePokemonsHealth()
+    {
+        foreach (var pokemon in this.pokemons)
+        {
+            pokemon.Health -= 10;
+        }
+        RemoveDeadPokemon();
+    }
+    private void RemoveDeadPokemon()
+    {
+        this.pokemons.RemoveAll(p => p.Health <= 0);
+    }
+    public override string ToString()
+    {
+        return $"{this.Name} {this.badges} {this.Pokemons.Count}";
+    }
+}
+
