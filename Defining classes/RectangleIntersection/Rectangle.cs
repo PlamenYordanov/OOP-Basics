12a297077af9be67fcfb6597131d25db3a07eea4
PlamenYordanov <yordanov.plamen@abv.bg>
2018-02-14 09:19:02 +0200

    rectangle intersection 80/100

---
+++ Defining classes/RectangleIntersection/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿public class Rectangle
+{
+    public string Id { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public Point Point { get; set; }
+    public Rectangle(string id, int width, int height, Point point)
+    {
+        this.Id = id;
+        this.Width = width;
+        this.Height = height;
+        this.Point = point;
+    }
+    public bool IsIntersected(Rectangle other)
+    {
+        if (this.Point.Y + this.Width < other.Point.Y
+            || other.Point.Y + other.Width < this.Point.Y
+            || this.Point.X + Height < other.Point.X
+            || other.Point.X + Height < this.Point.X)
+        {
+            return false;
+        }
+        return true;
+    }
+}
+
---
+++ Defining classes/RectangleIntersection/RectangleIntersectionStartup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RectangleIntersectionStartup
+{
+    public static void Main()
+    {
+        var nAndM = Console.ReadLine().Split();
+        int n = int.Parse(nAndM[0]);
+        int m = int.Parse(nAndM[1]);
+        if (n % 2 !=0)
+        {
+            return;
+        }
+        var rectangles = new List<Rectangle>();
+        for (int i = 0; i < n; i++)
+        {
+            var input = Console.ReadLine().Split();
+            string id = input[0];
+            int width = int.Parse(input[1]);
+            int height = int.Parse(input[2]);
+            int x = int.Parse(input[3]);
+            int y = int.Parse(input[4]);
+            var rectangle = new Rectangle(id, width, height, new Point(x, y));
+            rectangles.Add(rectangle);
+
+        }
+        
+        for (int i = 0; i < m; i++)
+        {
+            var input = Console.ReadLine().Split();
+            var thisRectangle = rectangles.SingleOrDefault(r => r.Id == input[0]);
+            var otherRectangle = rectangles.SingleOrDefault(r => r.Id == input[1]);
+            Console.WriteLine(thisRectangle.IsIntersected(otherRectangle).ToString().ToLower());
+        }
+
+    }
+}
+
